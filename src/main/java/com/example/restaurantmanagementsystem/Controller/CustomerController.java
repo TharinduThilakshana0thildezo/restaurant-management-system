c303097ffad77e30aa9de9a6481f9e8cf9b5fadf
Ishan Avishka <ishanavishka@Ishans-MacBook-Pro.local>
2025-05-15 19:48:52 +0530

    Update customer-management feature

---
+++ src/main/java/com/example/restaurantmanagementsystem/Controller/CustomerController.java
@@ -0,0 +1,32 @@
+package com.example.restaurantmanagementsystem.Controller;
+
+import com.example.restaurantmanagementsystem.model.Customer;
+import com.example.restaurantmanagementsystem.Service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/customers")
+@CrossOrigin(origins = "*")
+public class CustomerController {
+
+    @Autowired
+    private CustomerService service;
+
+    @GetMapping
+    public List<Customer> getAll() {
+        return service.getAllCustomers();
+    }
+
+    @PostMapping
+    public Customer addCustomer(@RequestBody Customer customer) {
+        return service.saveCustomer(customer);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCustomer(@PathVariable Long id) {
+        service.deleteCustomer(id);
+    }
+}
